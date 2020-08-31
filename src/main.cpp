7822228e44948821df7d651a8eb18968c2b81a01
Daniel Souza de Campos <danielcampos14@hotmail.com.br>
2020-08-30 21:18:54 -0300

    MedicaoComentada

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,23 @@
 #include <exception>
 #include <stdexcept>
 #include <iostream>
-#include <chrono>
+//#include <chrono>
 #include "Jogo.hpp"
-#define NUM_VEZES 10
+//#define NUM_VEZES 10
 
 int main(int argc, char *argv[]){
 	if(argc>1){
-		for(unsigned int i = 0;i<NUM_VEZES;i++){
-			auto start = std::chrono::high_resolution_clock::now();
-			std::string arq_entrada = argv[1];
-			Jogo::Jogo* jogo = new Jogo::Jogo();
-			jogo->configuraJogo(arq_entrada);
-			jogo->ganhador();
-			delete jogo;
-			auto stop = std::chrono::high_resolution_clock::now();
-			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-			std::cout<<"Duracao: "<<duration.count()<<std::endl;
-		}
+		// for(unsigned int i = 0;i<NUM_VEZES;i++){
+			 	// auto start = std::chrono::high_resolution_clock::now();
+				std::string arq_entrada = argv[1];
+				Jogo::Jogo* jogo = new Jogo::Jogo();
+				jogo->configuraJogo(arq_entrada);
+				jogo->ganhador();
+				delete jogo;
+				// auto stop = std::chrono::high_resolution_clock::now();
+				// auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+				// std::cout<<"Duracao: "<<duration.count()<<std::endl;
+			// }
 		
 	}else{
 		std::cout<< "Nao foi detectado nenhum arquivo de entrada"<<std::endl;
